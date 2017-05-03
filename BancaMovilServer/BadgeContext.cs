d32f02dc2dddeaa3c0b61ce3921942e0fb7697a2
Ramon Rodriguez <ramon.aris.rodriguez@gmail.com>
2017-05-02 23:30:55 -0400

    Include Binary and standart a db location

--- BancaMovilServer/BadgeContext.cs
+++ BancaMovilServer/BadgeContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Common;
+using System.Data.SQLite;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -50,11 +51,53 @@ namespace BancaMovilServer
         private static PetaPoco.Database GetDatabase()
         {
             // A sqlite database is just a file.
-            String fileName = Path.Combine(Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments), "badges.db");
-            String connectionString = "Data Source=" + fileName;
+            String fileName = "badges.db";
+            if (!File.Exists(fileName))
+            {
+                SeedDatabase(fileName);
+            }
+            String connectionString = String.Format("Data Source={0}", fileName);
             DbProviderFactory sqlFactory = new System.Data.SQLite.SQLiteFactory();
             PetaPoco.Database db = new PetaPoco.Database(connectionString, sqlFactory);
             return db;
+        }
+
+        private static void SeedDatabase(string fileName)
+        {
+            string dbConnection = String.Format("Data Source={0}", fileName);
+            String sql = @"
+                create table [Badges] (
+                [Id] INTEGER PRIMARY KEY ASC,
+                [Title] varchar(20) ,
+                [Description] varchar(255),
+                [Level] int)";
+            ExecuteNonQuery(dbConnection, sql);
+
+            sql = @"insert into Badges ([Title], [Description], [Level]) 
+             values ('Site MVP', 'Awarded to members who contribute often and wisely', 2);";
+            ExecuteNonQuery(dbConnection, sql);
+        }
+
+        private static int ExecuteNonQuery(string dbConnection, string sql)
+        {
+            SQLiteConnection cnn = new SQLiteConnection(dbConnection);
+            try
+            {
+                cnn.Open();
+                SQLiteCommand mycommand = new SQLiteCommand(cnn);
+                mycommand.CommandText = sql;
+                int rowsUpdated = mycommand.ExecuteNonQuery();
+                return rowsUpdated;
+            }
+            catch (Exception fail)
+            {
+                Console.WriteLine(fail.Message);
+                return 0;
+            }
+            finally
+            {
+                cnn.Close();
+            }
         }
     }
 }
